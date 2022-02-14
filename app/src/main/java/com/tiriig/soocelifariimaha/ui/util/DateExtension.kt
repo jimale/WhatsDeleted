eb6fe127d905160db6d82f7bb0c3b93683fb826e
Jimale <jimcalecabdi@gmail.com>
2022-02-14 15:19:11 +0300

    Removed slash

--- app/src/main/java/com/tiriig/soocelifariimaha/ui/util/DateExtension.kt
+++ app/src/main/java/com/tiriig/soocelifariimaha/ui/util/DateExtension.kt
@@ -5,5 +5,5 @@ import java.util.*
 
 fun Long.getTime(): String {
 // or you already have long value of date, use this instead of milliseconds variable.
-    return DateFormat.format("MM/dd/yyyy/HH/mm/ss", Date(this)).toString()
+    return DateFormat.format("MM/dd/yyyy HH/mm/ss", Date(this)).toString()
 }
