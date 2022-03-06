b267a5608ff9caebb1bd614fdd3eb416d96fc0d3
Jimale <jimcalecabdi@gmail.com>
2022-03-06 18:18:24 +0300

    Ignoring Notifications from other Apps

--- app/src/main/java/com/tiriig/soocelifariimaha/services/NotificationListener.kt
+++ app/src/main/java/com/tiriig/soocelifariimaha/services/NotificationListener.kt
@@ -5,8 +5,7 @@ import android.os.IBinder
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 
-
-class NotificationListener: NotificationListenerService() {
+class NotificationListener : NotificationListenerService() {
 
     override fun onBind(intent: Intent?): IBinder? {
         return super.onBind(intent)
@@ -14,10 +13,16 @@ class NotificationListener: NotificationListenerService() {
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
+        if (sbn != null && sbn.packageName == "com.whatsapp") {
+            sendMessage(sbn)
+        }
+    }
+
+    private fun sendMessage(sbn: StatusBarNotification) {
         val intent = Intent("com.tiriig.soocelifariimaha")
 
         var ticker = ""
-        if (sbn!!.notification.tickerText != null) {
+        if (sbn.notification.tickerText != null) {
             ticker = sbn.notification.tickerText.toString()
         }
         val extras = sbn.notification.extras
@@ -28,16 +33,22 @@ class NotificationListener: NotificationListenerService() {
         intent.putExtra("time", sbn.postTime)
         intent.putExtra("user", title)
         intent.putExtra("text", text)
+        intent.putExtra("id", sbn.key)
 
         sendBroadcast(intent)
-
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification?) {
         super.onNotificationRemoved(sbn)
+        if (sbn != null && sbn.packageName == "com.whatsapp") {
+            sendDeletedMessage(sbn)
+        }
+    }
+
+    private fun sendDeletedMessage(sbn: StatusBarNotification) {
         val intent = Intent("com.tiriig.soocelifariimaha")
 
-        val extras = sbn!!.notification.extras
+        val extras = sbn.notification.extras
         val title = extras.getString("android.title")
         val text = extras.getCharSequence("android.text").toString()
 
