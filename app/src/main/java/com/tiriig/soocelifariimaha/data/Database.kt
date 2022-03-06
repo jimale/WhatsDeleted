aa6e20fe6c2724581a59a954a9522a73ef783e85
Jimale <jimcalecabdi@gmail.com>
2022-03-06 17:56:10 +0300

    Added Converter

--- app/src/main/java/com/tiriig/soocelifariimaha/data/Database.kt
+++ app/src/main/java/com/tiriig/soocelifariimaha/data/Database.kt
@@ -2,14 +2,16 @@ package com.tiriig.soocelifariimaha.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.tiriig.soocelifariimaha.data.model.Message
+import androidx.room.TypeConverters
+import com.tiriig.soocelifariimaha.data.model.Chat
 
 @Database(
-    entities = [Message::class],
+    entities = [Chat::class],
     version = 9,
     exportSchema = true
 )
 
+@TypeConverters(Converters::class)
 abstract class Database : RoomDatabase() {
     abstract fun userDao(): UserDao
 }
