1243df03bc554458316c0008121b22edfb0b6c43
Jimale <jimcalecabdi@gmail.com>
2022-03-07 18:43:23 +0300

    Removed chat

--- app/src/main/java/com/tiriig/soocelifariimaha/data/Database.kt
+++ app/src/main/java/com/tiriig/soocelifariimaha/data/Database.kt
@@ -3,12 +3,11 @@ package com.tiriig.soocelifariimaha.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.tiriig.soocelifariimaha.data.model.Chat
 import com.tiriig.soocelifariimaha.data.model.Message
 
 @Database(
-    entities = [Chat::class,Message::class],
-    version = 9,
+    entities = [Message::class],
+    version = 1,
     exportSchema = true
 )
 
