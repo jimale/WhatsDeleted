eb629d46d923de5c00d27dfb68d28afe67966d1d
Jimale <jimcalecabdi@gmail.com>
2022-03-08 23:33:39 +0300

    Removed GlobalScope and added ProcessLifecycleOwner

--- app/src/main/java/com/tiriig/soocelifariimaha/services/NLServiceReceiver.kt
+++ app/src/main/java/com/tiriig/soocelifariimaha/services/NLServiceReceiver.kt
@@ -3,12 +3,12 @@ package com.tiriig.soocelifariimaha.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import com.tiriig.soocelifariimaha.data.model.Chat
 import com.tiriig.soocelifariimaha.data.repository.ChatRepository
 import com.tiriig.soocelifariimaha.ui.util.getRandomNum
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,9 +16,8 @@ import javax.inject.Inject
 class NLServiceReceiver : BroadcastReceiver() {
 
     @Inject
-    lateinit var repository : ChatRepository
+    lateinit var repository: ChatRepository
 
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onReceive(context: Context?, intent: Intent) {
         val user = intent.getStringExtra("user") ?: ""
         val text = intent.getStringExtra("text") ?: ""
@@ -27,7 +26,8 @@ class NLServiceReceiver : BroadcastReceiver() {
 
         //Save message to the database
         val chat = Chat(id, user, text, time)
-        GlobalScope.launch {
+        // ProcessLifecycleOwner provides lifecycle for the whole application process.
+        ProcessLifecycleOwner.get().lifecycleScope.launch {
             repository.saveMessage(chat)
         }
     }
