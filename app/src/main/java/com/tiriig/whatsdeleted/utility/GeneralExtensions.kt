5a767673159ae08ff2597fb4b02c60f64dbe36a0
Jimale <jimcalecabdi@gmail.com>
2022-04-21 02:52:01 +0300

    Added new title to ignore

--- app/src/main/java/com/tiriig/whatsdeleted/utility/GeneralExtensions.kt
+++ app/src/main/java/com/tiriig/whatsdeleted/utility/GeneralExtensions.kt
@@ -8,6 +8,7 @@ fun String.isValidTitle(): Boolean {
         "WhatsApp" -> false
         "Calling..." -> false
         "Ringing.." -> false
+        "Deleting messages.." -> false
         "Ongoing voice call" -> false
         "WhatsApp Web" -> false
         "Finished backup" -> false
