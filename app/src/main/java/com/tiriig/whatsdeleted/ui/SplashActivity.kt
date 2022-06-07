6c34ec57629343d636cf0afa1d2a963a88d184f3
Jimale <jimcalecabdi@gmail.com>
2022-06-07 11:14:42 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/tiriig/whatsdeleted/ui/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.tiriig.whatsdeleted.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+
+    override fun onStart() {
+        super.onStart()
+        startMainActivity()
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        finish()
+    }
+
+}
